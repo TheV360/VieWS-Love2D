3ee5705ff143fcc6539a87c70a84a85fda049961
V360 <v360code@gmail.com>
2022-01-04 02:17:37 -0500

    radical close window thing

--- views/effects/closeWindow.lua
+++ views/effects/closeWindow.lua
@@ -1,11 +1,12 @@
 local CloseWindow = Effects.Effect:extend()
 
-CloseWindow.maxLife = 15
+CloseWindow.maxLife = 120
 
 function CloseWindow:new(o)
 	CloseWindow.super.new(self, o)
 	
 	self.size = Size(o.width or 8, o.height or 8)
+	self.fallDir = (math.random() > 0.5 and 1 or -1) * (1 + math.floor(math.random() * 10 + 0.5) / 10)
 	
 	self.life = CloseWindow.maxLife
 end
@@ -13,14 +14,16 @@ end
 function CloseWindow:draw()
 	local p = 1 - (self.life / CloseWindow.maxLife)
 	
-	love.graphics.setColor(VieWS.PALETTE[1])
-	love.graphics.rectangle(
-		"line",
-		Util.lerp(self.position.x, self.position.x + self.size.x / 2, p),
-		Util.lerp(self.position.y, self.position.y + self.size.y / 2, p),
-		Util.lerp(self.size.x, 0, p),
-		Util.lerp(self.size.y, 0, p)
-	)
+	love.graphics.setColor(VieWS.PALETTE[Util.round(Util.lerp(4, 1, p))])
+	love.graphics.push()
+		local fall = Vec2(p * self.fallDir, math.sin(math.pi * p * 1.25) * -60)
+		local pos = self.position + self.size / 2 + fall
+		local scale = Vec2.lerp(self.size, Vec2(0), p)
+		love.graphics.translate(pos:unpack())
+		love.graphics.rotate(p * p * self.fallDir * 4)
+		love.graphics.scale(scale:unpack())
+		love.graphics.rectangle("fill", -0.5, -0.5, 1, 1)
+	love.graphics.pop()
 end
 
 return CloseWindow
