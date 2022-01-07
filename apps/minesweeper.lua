7a8c0f3ee60ca33ab2345561c0ddceb1d5676a07
V360 <v360code@gmail.com>
2022-01-07 01:26:05 -0500

    basic work on minesweeper

--- apps/minesweeper.lua
+++ apps/minesweeper.lua
@@ -1,13 +1,18 @@
 local Minesweeper = {}
 
-Minesweeper.Images = {
-	
-}
-
 function Minesweeper.setup()
-	view:addWindow(Controls.Window{
+	local cfg = {
+		statusBar = 12,
+		cellSize = Vec2(12, 12),
+		gridSize = Vec2(8, 8),
+	}
+	cfg.totalSize = cfg.cellSize * cfg.gridSize + Vec2(0, statusBar)
+	
+	local w = Controls.Window {
 		title = "Minesweeper",
-	})
+		size = cfg.totalSize
+	}
+	view:addWindow(w)
 end
 
 return Minesweeper
