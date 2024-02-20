7ad104373d505793315baf9ee93fdc54d088de17
V360 <v360code@gmail.com>
2024-02-19 19:27:54 -0500

    fix label errors

--- views/controls/label.lua
+++ views/controls/label.lua
@@ -4,14 +4,20 @@ function Label:new(o)
 	self.text = o.text or "Label"
 	self.color = o.color or 1
 	
-	if not (o.size and o.size.y or o.height) then
-		local wrapWidth, wrapText = font:getWrap(o.text, o.maxWidth or o.width)
-		o.width = wrapWidth
-		o.height = #(wrapText or "1") * font:getHeight()
+	o.size = o.size or Vec2(o.width or -1, o.height or -1)
+	
+	-- If width was left unspecified, figure it out from the given text.
+	if o.size.x < 0 then
+		o.size.x = font:getWidth(o.text)
+		o.size.y = font:getHeight()
 	end
-	if not (o.size and o.size.x or o.width ) then
-		o.width = font:getWidth(o.text)
-		o.height = font:getHeight()
+	
+	-- If width was given but height was left unspecified,
+	-- wrap the text to fill the width, and figure out the height from that.
+	if o.size.y < 0 then
+		local wrapWidth, lines = font:getWrap(o.text, o.maxWidth or o.size.x)
+		o.size.x = wrapWidth
+		o.size.y = (lines and #lines or 1) * font:getHeight()
 	end
 	
 	Label.super.new(self, o)
